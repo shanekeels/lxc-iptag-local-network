8658db6e2398d2be6c5cc3eab06d061912bd6537
shanekeels <skeels@g.clemson.edu>
2024-09-19 18:59:12 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 set -xe
 
 apt install -y ipcalc
-curl -sSL https://raw.githubusercontent.com/gitsang/lxc-iptag/main/lxc-iptag -o /usr/local/bin/lxc-iptag
-curl -sSL https://raw.githubusercontent.com/gitsang/lxc-iptag/main/lxc-iptag.service -o /lib/systemd/system/lxc-iptag.service
+curl -sSL https://raw.githubusercontent.com/shanekeels/lxc-iptag-local-network/main/lxc-iptag -o /usr/local/bin/lxc-iptag
+curl -sSL https://raw.githubusercontent.com/shanekeels/lxc-iptag-local-network/main/lxc-iptag.service -o /lib/systemd/system/lxc-iptag.service
 chmod +x /usr/local/bin/lxc-iptag
 
 systemctl daemon-reload
